6f1fd55dcd85e28f155d3247ec6ca40a634976fc
liborhavranek <liborhavranek91@gmail.com>
2022-11-28 23:26:43 +0100

    trophy complete booblehead still not done

---
+++ myscript.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+$(".hide").hide();  
+  $(".meele-booblehead").click(function(){
+    $(".meele-booblehead-description").toggle();
+  });
+
+  $(".explosive-booblehead").click(function(){
+    $(".explosive-booblehead-description").toggle();
+  });
+
+  $(".sneak-booblehead").click(function(){
+    $(".sneak-booblehead-description").toggle();
+  });
+
+  $(".barter-booblehead").click(function(){
+    $(".barter-booblehead-description").toggle();
+  });
+
+  $(".perception-booblehead").click(function(){
+    $(".perception-booblehead-description").toggle();
+  });
+
+  $(".strength-booblehead").click(function(){
+    $(".strength-booblehead-description").toggle();
+  });
+
+  $(".speech-booblehead").click(function(){
+    $(".speech-booblehead-description").toggle();
+  });
+
+  $(".charisma-booblehead").click(function(){
+    $(".charisma-booblehead-description").toggle();
+  });
+
+  $(".lock-booblehead").click(function(){
+    $(".lock-booblehead-description").toggle();
+  });
+
+  $(".smallguns-booblehead").click(function(){
+    $(".smallguns-booblehead-description").toggle();
+  });
+
+  $(".bigguns-booblehead").click(function(){
+    $(".bigguns-booblehead-description").toggle();
+  });
+
+  $(".medicine-booblehead").click(function(){
+    $(".medicine-booblehead-description").toggle();
+  });
+
+  $(".intelligence-booblehead").click(function(){
+    $(".intelligence-booblehead-description").toggle();
+  });
+
+  $(".endurance-booblehead").click(function(){
+    $(".endurance-booblehead-description").toggle();
+  });
+
+  $(".unarmed-booblehead").click(function(){
+    $(".unarmed-booblehead-description").toggle();
+  });
+
+  $(".agility-booblehead").click(function(){
+    $(".agility-booblehead-description").toggle();
+  });
+
+  $(".energyweapon").click(function(){
+    $(".energyweapon-booblehead-description").toggle();
+  });
+
+  $(".luck-booblehead").click(function(){
+    $(".luck-booblehead-description").toggle();
+  });
+
+  $(".repair-booblehead").click(function(){
+    $(".repair-booblehead-description").toggle();
+  });
+
+  $(".science-booblehead").click(function(){
+    $(".science-booblehead-description").toggle();
+  });
+
+});
