18646c6d5e2c9328aa14fccfa12faa0ad5e50d87
liborhavranek <liborhavranek91@gmail.com>
2022-11-29 16:56:12 +0100

    boobleheads map stale neni dokonceno

--- myscript.js
+++ myscript.js
@@ -1,83 +1,105 @@
 $(document).ready(function(){
-$(".hide").hide();  
+  $(".hide").hide();  
+
   $(".meele-booblehead").click(function(){
+      $(".hide").hide();  
     $(".meele-booblehead-description").toggle();
   });
 
   $(".explosive-booblehead").click(function(){
+      $(".hide").hide();  
     $(".explosive-booblehead-description").toggle();
   });
 
   $(".sneak-booblehead").click(function(){
+      $(".hide").hide();  
     $(".sneak-booblehead-description").toggle();
   });
 
   $(".barter-booblehead").click(function(){
+      $(".hide").hide();  
     $(".barter-booblehead-description").toggle();
   });
 
   $(".perception-booblehead").click(function(){
+      $(".hide").hide();  
     $(".perception-booblehead-description").toggle();
   });
 
   $(".strength-booblehead").click(function(){
+      $(".hide").hide();  
     $(".strength-booblehead-description").toggle();
   });
 
   $(".speech-booblehead").click(function(){
+      $(".hide").hide();  
     $(".speech-booblehead-description").toggle();
   });
 
   $(".charisma-booblehead").click(function(){
+      $(".hide").hide();  
     $(".charisma-booblehead-description").toggle();
   });
 
   $(".lock-booblehead").click(function(){
+      $(".hide").hide();  
     $(".lock-booblehead-description").toggle();
   });
 
   $(".smallguns-booblehead").click(function(){
+      $(".hide").hide();  
     $(".smallguns-booblehead-description").toggle();
   });
 
   $(".bigguns-booblehead").click(function(){
+      $(".hide").hide();  
     $(".bigguns-booblehead-description").toggle();
   });
 
   $(".medicine-booblehead").click(function(){
-    $(".medicine-booblehead-description").toggle();
+      $(".hide").hide();  
+    $(".medicine-booblehead-description").toggle(); 
   });
 
   $(".intelligence-booblehead").click(function(){
+      $(".hide").hide();  
     $(".intelligence-booblehead-description").toggle();
   });
 
   $(".endurance-booblehead").click(function(){
+      $(".hide").hide();  
     $(".endurance-booblehead-description").toggle();
   });
 
   $(".unarmed-booblehead").click(function(){
+      $(".hide").hide();  
     $(".unarmed-booblehead-description").toggle();
   });
 
   $(".agility-booblehead").click(function(){
+      $(".hide").hide();  
     $(".agility-booblehead-description").toggle();
   });
 
   $(".energyweapon").click(function(){
+      $(".hide").hide();  
     $(".energyweapon-booblehead-description").toggle();
   });
 
   $(".luck-booblehead").click(function(){
+      $(".hide").hide();  
     $(".luck-booblehead-description").toggle();
   });
 
   $(".repair-booblehead").click(function(){
+      $(".hide").hide();  
     $(".repair-booblehead-description").toggle();
   });
 
   $(".science-booblehead").click(function(){
+      $(".hide").hide();  
     $(".science-booblehead-description").toggle();
   });
 
 });
+
